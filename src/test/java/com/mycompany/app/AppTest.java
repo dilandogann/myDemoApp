cc6188f58a8f1539130a70d3b04275645cb1c1df
dilan dogan <d.dogan@etu.edu.tr>
2018-10-09 22:34:51 +0300

    new commit

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -44,7 +44,7 @@ public class AppTest
     
     public void testSumTrueMyMethod(){
     
-     ArrayList<Integer> array=new Arraylist<>(Arrays.asList(1,2,3,4,));
+     ArrayList<Integer> array=new Arraylist<>(Arrays.asList(1,2,3,4));
      ArrayList<Integer> array2=new ArrayList<>(Arrays.asList(2,3,4,5));
      assertTrue(new App().myMethod(array,10,array2,14));
     
